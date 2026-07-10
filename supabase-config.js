d4bda51bad85575b19935266ba1a131dec41d671
yukyikblue <yukyik.bluemoon@gmail.com>
2026-07-10 17:00:30 +0000

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 const SUPABASE_URL = 'https://pwrhnmvhwhellfbznczb.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_zmIZ9aucZsRMJrySDe0uIQ_W4OgndeO';
 
-// Initialize Supabase client
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// Initialize Supabase client (เปลี่ยนชื่อจาก supabase เป็น supabaseClient)
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
