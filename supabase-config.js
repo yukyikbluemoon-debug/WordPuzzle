6c4720edfa5b59fcf412d14462dd83ebe418344e
yukyikblue <yukyik.bluemoon@gmail.com>
2026-07-11 13:53:31 +0000

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-// Supabase Configuration
-const SUPABASE_URL = 'https://pwrhnmvhwhellfbznczb.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_zmIZ9aucZsRMJrySDe0uIQ_W4OgndeO';
-
-// Initialize Supabase client
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
